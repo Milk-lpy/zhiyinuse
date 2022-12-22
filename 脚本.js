f953a09707e4205f3621aa3bdb78f0cee3205326
Hero。 <81848987+Milk-lpy@users.noreply.github.com>
2022-12-22 15:16:26 +0800

    Update 脚本.js

--- 脚本.js
+++ 脚本.js
@@ -16,20 +16,3 @@ const headers = {"Host": "yqfk.cqepc.edu.cn"
 "Content-Length": "979"};
 const data = {"isTravel":"0","isContact":"0","isCohabit":"0","isFatigue":"0","isShortnessBreath":"0","travelInfoVo":{"relationshipInfo":[{"contactInfo":"","name":"","relationship":"","key":"0"}]},"contactInfo":{},"cohabitInfo":[{"name":"","province,city,disctrict":"","returnTime":"","transType":"","transTypeValue":"","transNumber":"","currentDetailResidence":"","contactInfo":"0","highRiskInfo":"0","governmentQuarantine":"0","homeQuarantine":"0","healthCardInfo":"","healthCardUrl":"","travelInfo":"","travelUrl":"","nucleicAcid":"","nucleicAcidMethod":"","nucleicAcidPointName":"","nucleicAcidSamplingDate":"","nucleicAcidAgency":"","nucleicAcidTime":"","nucleicAcidResult":"","nucleicAcidImg":"","key":"0"}],"healthCardInfo":"0","healthStatus":"0","travelInfo":"0","locateDetailedAddress":"山东省烟台市栖霞市庄园街道卧龙村民安置房","locateLatitude":"37.33592","locateLongitude":"120.84956","city":"烟台市","disctrict":"栖霞市","cityCode":"0535","disctrictCode":"370686","province":"山东省"};
 
-const myRequest = {
-    url: url,
-    method: post, // Optional, default GET.
-    headers: headers, // Optional.
-    body: JSON.stringify(data) // Optional.
-};
-
-$task.fetch(myRequest).then(response => {
-   response.body
-    console.log(response.body);
-    $notify("Title", "Subtitle", response.body); // Success!
-    $done();
-}, reason => {
-    // reason.error
-    $notify("Title", "Subtitle", reason.error); // Error!
-    $done();
-});
